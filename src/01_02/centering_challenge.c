8c0dcd745187efaf505ed0aa4e320f76538fb052
lila00245 <165945521+lila00245@users.noreply.github.com>
2026-01-06 18:13:14 +0000

    finished centering challenge!

--- src/01_02/centering_challenge.c
+++ src/01_02/centering_challenge.c
@@ -2,30 +2,32 @@
 
 void center_text(int width, char *text)
 {
-	/*
-	   - write this function to complete the challenge
-	   - this code won't run until the function is
-	   written
-	*/
+	int text_len = strlen(text);
+	int indent = (width - text_len) / 2;
+
+	for (int i = 0; i < indent; i++) {
+		putchar(' ');
+	}
+	
+	printf("%s\n", text);
 }
 
 int main()
 {
 	/* some sample text to center */
 	char *title[] = {
-	   "March Sales",
-	   "My First Project",
-	   "Centering output is so much fun!",
-	   "This title is very long, just to see whether the code can handle such a long title"
-	};
+			"March Sales",
+			"My First Project",
+			"Centering output is so much fun!",
+			"This title is very long, just to see whether the code can handle such a long title"};
 	int x;
 
 	/* output each of the strings, centered */
-	for( x=0; x<4; x++ )
+	for (x = 0; x < 4; x++)
 	{
 		/* assume the screen width to be 80 characters */
-		center_text( 80, title[x] );
+		center_text(80, title[x]);
 	}
 
-	return(0);
+	return (0);
 }
